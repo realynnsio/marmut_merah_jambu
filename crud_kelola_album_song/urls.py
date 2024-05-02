ebd8d5fcc1dfb1f083b80199bf9ce690cb3aaa7b
jep <n.siomusic@gmail.com>
2024-05-02 07:33:39 +0700

    add: daftar lagu & create lagu

--- crud_kelola_album_song/urls.py
+++ crud_kelola_album_song/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from crud_kelola_album_song.views import show_list_album
+from crud_kelola_album_song.views import show_list_album, show_song_list_album
+from crud_kelola_album_song.views import create_song
 
 app_name = 'crud_kelola_album_song'
 
 urlpatterns = [
-    path('all/', show_list_album, name='show_list_album'),
+    path('', show_list_album, name='show_list_album'),
+    path('detail/', show_song_list_album, name='show_song_list_album'),
+    path('add-song/', create_song, name='create_song'),
 ]
--- crud_kelola_album_song/views.py
+++ crud_kelola_album_song/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def show_list_album(request):
     context = {}
     return render(request, "list_album.html", context)
+
+def show_song_list_album(request):
+    context = {}
+    return render(request, "daftar_lagu.html", context)
+
+def create_song(request):
+    context = {}
+    return render(request, "create_lagu.html", context)
