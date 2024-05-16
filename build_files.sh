2cfb49d00452fe20cec0f573c0d89ca08e3490ec
jep <n.siomusic@gmail.com>
2024-05-16 21:11:11 +0700

    deployment attempt #1

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-pip install -r requirements.txt
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+pip3 install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
