c549fdc83e04272101c18799ed4ac609439d0b6d
jep <n.siomusic@gmail.com>
2024-05-17 12:40:35 +0700

    admin fix

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Paket, Akun
+from .models import Paket, Akun, Konten, PemilikHakCipta
 
 # Register your models here.
 admin.site.register(Paket)
 admin.site.register(Akun)
+admin.site.register(Konten)
+admin.site.register(PemilikHakCipta)
