01e2c50a09d802ea9cf91feae7db92c683382481
jep <n.siomusic@gmail.com>
2024-05-17 12:39:46 +0700

    add more models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Paket
+from .models import Paket, Akun
 
 # Register your models here.
 admin.site.register(Paket)
+admin.site.register(Akun)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class Paket(models.Model):
     jenis = models.CharField(max_length=50, primary_key=True)
@@ -6,3 +7,38 @@ class Paket(models.Model):
 
     class Meta:
         db_table = 'paket'  # Specify the exact table name in the database
+
+class Akun(models.Model):
+    email = models.EmailField(primary_key=True)
+    password = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    GENDER_CHOICES = (
+        (0, 'Female'),
+        (1, 'Male'),
+    )
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    tempat_lahir = models.CharField(max_length=50)
+    tanggal_lahir = models.DateField()
+    is_verified = models.BooleanField()
+    kota_asal = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'akun'  # Specify the exact table name in the database
+
+
+class Konten(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    judul = models.CharField(max_length=100)
+    tanggal_rilis = models.DateField()
+    tahun = models.IntegerField()
+    durasi = models.IntegerField()
+
+    class Meta:
+        db_table = 'konten'  # Specify the exact table name in the database
+
+class PemilikHakCipta(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    rate_royalti = models.IntegerField()
+
+    class Meta:
+        db_table = 'pemilik_hak_cipta'
